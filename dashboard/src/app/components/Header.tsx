02e59c3c56f9392827fd93666fc02d178937f1a6
igachev <ivophoenix1135@gmail.com>
2024-06-04 17:59:07 +0300

    show Profile button only if the user is logged in

--- dashboard/src/app/components/Header.tsx
+++ dashboard/src/app/components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { auth } from '@clerk/nextjs/server';
+import { UserButton } from '@clerk/nextjs';
 
 const Header: React.FC = () => {
 
@@ -9,11 +10,16 @@ const Header: React.FC = () => {
     <>
     <nav className="bg-blue-700 py-4 px-6 flex items-center justify-center mb-5">
         <div className="text-white">
+
           {!userId && (
             <>
             <Link href="sign-in" className='text-gray-300 hover:text-white mr-4'>Sign In</Link>
             <Link href="sign-up" className='text-gray-300 hover:text-white mr-4'>Sign Up</Link>
             </>
+          )}
+
+          {userId && (
+            <UserButton afterSignOutUrl='/' />
           )}
             
         </div>
