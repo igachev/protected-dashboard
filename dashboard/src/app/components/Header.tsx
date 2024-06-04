f95740c5189a09c86549263a40fa7700737e22e8
igachev <ivophoenix1135@gmail.com>
2024-06-04 17:37:04 +0300

    show SignIn,SignUp only if we are logged out

--- dashboard/src/app/components/Header.tsx
+++ dashboard/src/app/components/Header.tsx
@@ -1,13 +1,21 @@
 import Link from 'next/link'
-import React from 'react'
+import { auth } from '@clerk/nextjs/server';
 
 const Header: React.FC = () => {
+
+  const { userId } : { userId: string | null } = auth();
+
   return (
     <>
     <nav className="bg-blue-700 py-4 px-6 flex items-center justify-center mb-5">
         <div className="text-white">
+          {!userId && (
+            <>
             <Link href="sign-in" className='text-gray-300 hover:text-white mr-4'>Sign In</Link>
             <Link href="sign-up" className='text-gray-300 hover:text-white mr-4'>Sign Up</Link>
+            </>
+          )}
+            
         </div>
     </nav>
     </>
