2f94ad5015caf598c32ca64eda6ab789fbdcf321
igachev <ivophoenix1135@gmail.com>
2024-06-04 23:43:38 +0300

    added the date picker

--- dashboard/src/app/dashboard/page.tsx
+++ dashboard/src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-
+"use client";
 
 import {
   Card,
@@ -11,6 +11,22 @@ import {
 
 import { Button } from "@/components/ui/button"
 
+import * as React from "react"
+import { addDays, format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { DateRange } from "react-day-picker"
+ 
+import { cn } from "@/lib/utils"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+
+
+
 
 const DashboardPage: React.FC = () => {
   return (
@@ -20,7 +36,9 @@ const DashboardPage: React.FC = () => {
     <div className="flex justify-between items-center">
     <CardTitle>Dashboard</CardTitle>
     <div className="flex gap-3">
-    <CardDescription>Card Description</CardDescription>
+    <DatePickerWithRange />
+
+    
     <Button variant="default">Button</Button>
 
     </div>
@@ -38,5 +56,56 @@ const DashboardPage: React.FC = () => {
     </>
   )
 }
+
+export function DatePickerWithRange({
+  className,
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: new Date(2022, 0, 20),
+    to: addDays(new Date(2022, 0, 20), 20),
+  })
+ 
+  return (
+    <div className={cn("grid gap-2", className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"outline"}
+            className={cn(
+              "w-[300px] justify-start text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  )
+}
+
 
 export default DashboardPage
