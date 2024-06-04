d821ec9b2e3add35d288843ab37222e24a4b822a
igachev <ivophoenix1135@gmail.com>
2024-06-04 14:06:50 +0300

    added Header component
    It contains Sign In,Sign Up links

---
+++ dashboard/src/app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <nav className="bg-blue-700 py-4 px-6 flex items-center justify-center mb-5">
+        <div className="text-white">
+            <Link href="sign-in" className='text-gray-300 hover:text-white mr-4'>Sign In</Link>
+            <Link href="sign-up" className='text-gray-300 hover:text-white mr-4'>Sign Up</Link>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Header
--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "./components/Header"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,8 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
       <body className={inter.className}>
-        
+
+        <Header />
         <main className="container mx-auto">
           <div className="flex items-start justify-center min-h-screen">
             <div className="mt-20">
