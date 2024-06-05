54190783ea0cfbfa0fe1d795ed4b87d6cece635d
igachev <ivophoenix1135@gmail.com>
2024-06-05 14:36:26 +0300

    update next.config

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    publicRuntimeConfig: {
-        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
